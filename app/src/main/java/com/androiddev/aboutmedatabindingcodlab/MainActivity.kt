b110e2a5a972b2330eb94c34a10dcf1b0279f4cb
Mert Akdoğan <mert_1102@hotmail.com>
2022-01-20 14:44:51 +0300

    Commit

---
+++ app/src/main/java/com/androiddev/aboutmedatabindingcodlab/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.androiddev.aboutmedatabindingcodlab
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.androiddev.aboutmedatabindingcodlab.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Aleks Haecky")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myNameObject = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        binding. apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll() //bu yapıyı değiştirdek sonra UI ı güncelle refresh et
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        // Klavyeyi gizleme işlevi
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
